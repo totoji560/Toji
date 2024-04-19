5fc48b5a27b5724c81cf2a8bc44b75e1488575b5
Amkakira <164623285+Amkakira@users.noreply.github.com>
2024-04-19 23:23:34 +0200

    Add files via upload

--- plugins/مخفي.js
+++ plugins/مخفي.js
@@ -1,26 +1,16 @@
-import MessageType from '@adiwajshing/baileys'
-import { generateWAMessageFromContent } from '@adiwajshing/baileys'
+import { generateWAMessageFromContent } from '@whiskeysockets/baileys'
 
-let handler = async (m, { conn, text, participants }) => {
-let users = participants.map(u => conn.decodeJid(u.id))
-let q = m.quoted ? m.quoted : m
-let c = m.quoted ? m.quoted : m.msg
-const msg = conn.cMod(m.chat,
-generateWAMessageFromContent(m.chat, {
-[c.toJSON ? q.mtype : 'extendedTextMessage']: c.toJSON ? c.toJSON() : {
-text: c || ''
+let handler = async (m, { conn, text, participants, isOwner, isAdmin }) => {
+    let users = participants.map(u => conn.decodeJid(u.id))
+    let q = m.quoted ? m.quoted : m || m.text || m.sender
+    let c = m.quoted ? await m.getQuotedObj() : m.msg || m.text || m.sender
+    let msg = conn.cMod(m.chat, generateWAMessageFromContent(m.chat, { [m.quoted ? q.mtype : 'extendedTextMessage']: m.quoted ? c.message[q.mtype] : { text: '' || c }}, { quoted: m, userJid: conn.user.id }), text || q.text, conn.user.jid, { mentions: users })
+    await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
 }
-}, {
-quoted: m,
-userJid: conn.user.id
-}),
-text || q.text, conn.user.jid, { mentions: users }
-)
-await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
-}
-handler.help = ['hidetag']
+
+handler.help = ['مخفي']
 handler.tags = ['group']
-handler.command = ['hidetag', 'notify','مخفي'] 
+handler.command = /^(مخفي|وهمي)$/i
 handler.group = true
 handler.admin = true
 
---
+++ plugins/منشن.js
@@ -0,0 +1,20 @@
+let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
+if (!(isAdmin || isOwner)) {
+global.dfail('admin', m, conn)
+throw false
+}//غير ڤينوم و حط اسم بوتك
+let pesan = args.join` `
+let oi = `*❖┃الرساله,* ${pesan}`
+let teks = `  \n\n ${oi}\n\n *⏎ المـــنشـــن«✅»الجـــمـــاعـــي➥*  :⇣\n`
+for (let mem of participants) {
+teks += `*•┃➥ تفاعل ي حب⏎* @${mem.id.split('@')[0]}\n`}
+  
+teks += `*❂ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ*\n\n*❖━━━━━━[ 𝑁𝐸𝑍𝑈𝐾𝛩-𝐵𝛩𝑇 ]━━━━━❖*`
+conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
+}
+handler.help = ['tagall <mesaje>','invocar <mesaje>']
+handler.tags = ['group']
+handler.command = /^(منشن|invocar|invocacion|todos|invocación)$/i
+handler.admin = true
+handler.group = true
+export default handler
