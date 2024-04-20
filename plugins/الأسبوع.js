e80a9dd106f353ba77bfd101c698ccef549636ed
Amkakira <164623285+Amkakira@users.noreply.github.com>
2024-04-20 23:11:00 +0200

    Update الأسبوع.js

--- plugins/الأسبوع.js
+++ plugins/الأسبوع.js
@@ -1,30 +1,32 @@
 const rewards = {
- exp: 15000,
+    exp: 15000,
     money: 35999,
     potion: 9,
-  }
-  const cooldown = 604800000
-  let handler = async (m) => {
+}
+
+const cooldown = 604800000
+let handler = async (m) => {
     let user = global.db.data.users[m.sender]
-    if (new Date - user.lastweekly < cooldown) throw `You have already claimed this daily claim!, wait for *${((user.lastweekly + cooldown) - new Date()).toTimeString()}*`
+    if (Date.now() - user.lastweekly < cooldown) throw `You have already claimed this weekly claim, wait for *${((user.lastweekly + cooldown) - Date.now()).toTimeString()}*`
     let text = ''
     for (let reward of Object.keys(rewards)) {
-      if (!(reward in user)) continue
-      user[reward] += rewards[reward]
-      text += `*+${rewards[reward]}* ${global.rpg.emoticon(reward)}${reward}\n`
+        if (!(reward in user)) continue
+        user[reward] += rewards[reward]
+        text += `*+${rewards[reward]}* ${global.rpg.emoticon(reward)}${reward}\n`
     }
     //conn.sendButton(m.chat,'*––––––『 WEEKLY 』––––––*', text.trim(), null, [['Profile', ''], ['weekly', '.weekly']],m)
     m.reply(`
     🎁 *weekly reward*
     
-    ▢ *Has recieved:*
+    ▢ *Has received:*
      ${text}`)
-    user.lastweekly = new Date * 1
-  }
-  handler.help = ['weekly']
-  handler.tags = ['econ']
-  handler.command = /^(اسبوعي)$/i
-  
-  handler.cooldown = cooldown
-  
-  export default handler
+    user.lastweekly = Date.now()
+}
+
+handler.help = ['weekly']
+handler.tags = ['econ']
+handler.command = /^(اسبوعي)$/i
+
+handler.cooldown = cooldown
+
+export default handler
