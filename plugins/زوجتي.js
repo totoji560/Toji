19d96a21661e73e82b0c2b1f056cebfc72f741a1
Amkakira <164623285+Amkakira@users.noreply.github.com>
2024-04-21 09:22:39 +0200

    Create زوجتي.js

---
+++ plugins/زوجتي.js
@@ -0,0 +1,16 @@
+let toM = a => '@' + a.split('@')[0]
+function handler(m, { groupMetadata }) {
+let ps = groupMetadata.participants.map(v => v.id)
+let a = ps.getRandom()
+let b
+do b = ps.getRandom()
+while (b === a)
+let user = m.sender;
+m.reply(`@${user.split('@')[0]}  خد زوجتك و غور من هنا*  ${toM(b)}, 💓`, null, {
+mentions: [user, b]
+})}
+handler.help = ['formarpareja']
+handler.tags = ['main', 'fun']
+handler.command = ['زوجني','زوجتي']
+handler.group = true
+export default handler
