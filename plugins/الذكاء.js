2b6f1320d51455a2ff0c6d185b41a669e1eb8ac2
Amkakira <164623285+Amkakira@users.noreply.github.com>
2024-05-04 13:03:02 +0300

    Update الذكاء.js

--- plugins/الذكاء.js
+++ plugins/الذكاء.js
@@ -1,10 +1,12 @@
 let handler = async (m, { conn, command, text }) => {
-let intelligence = `*💡 نسبة الذكاء 💡*
-*نسبة ذكاء ${text} 💡هي* *${Math.floor(Math.random() * 100)}%* *من 100%*
-*ربنا يشفيك😂❤*
+let intelligence = `*┃❐━═━═✦◈❨💡❩◈✦═━═━❐┃*
+
+*⏎┇نسبة ذكائــك ${text} هي ${Math.floor(Math.random() * 100)}%┇☞*
+*⏎┇دي درجـــة حـــرارة ربـنـا يـشـفـيـك┇➥*
 `.trim()
 m.reply(intelligence, null, { mentions: conn.parseMention(intelligence) })}
 handler.help = ['intelligence']
 handler.tags = ['fun']
 handler.command = /^(ذكاء)$/i
 export default handler
+
