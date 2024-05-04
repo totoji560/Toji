017ac037a9e758946ea0e9b4ae493db2c1bab08d
Amkakira <164623285+Amkakira@users.noreply.github.com>
2024-05-04 13:28:26 +0300

    Update طرد.js

--- plugins/طرد.js
+++ plugins/طرد.js
@@ -1,18 +1,18 @@
 let handler = async (m, { conn, participants, usedPrefix, command }) => {
 	
-let kickte = `*منشن ع الشخص الي هتطرده بره يا حب🥺❤*`
+let kickte = `*⧉┇منشن ع المستخدم الي هتطردو┇〄*`
 
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`*تم وديته الجحيم يا سيدي اكيرا*`) 
+m.reply(`*〄┇لــقــد ذهــب الــي الــجـــحـ🔥ـيــم┇⟤*`) 
 
 }
 
 handler.help = ['kick @user']
 handler.tags = ['group']
-handler.command = ['kick', 'طرد'] 
+handler.command = ['برا', 'طرد'] 
 handler.admin = true
 handler.group = true
 handler.botAdmin = true
