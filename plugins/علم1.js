51519058c3ba1659d81c88be37170166497dc5c1
Amkakira <164623285+Amkakira@users.noreply.github.com>
2024-05-04 13:29:00 +0300

    Update علم1.js

--- plugins/علم1.js
+++ plugins/علم1.js
@@ -11,11 +11,11 @@ let handler = async (m, { conn, command, usedPrefix }) => {
     }
     let src = await (await fetch('https://raw.githubusercontent.com/ze819/game/master/src/game.js/luffy1.json')).json()
   let json = src[Math.floor(Math.random() * src.length)]
-    let caption = `*❰❖── ~『𝑁𝐸𝑍𝑈𝐾𝛩–𝐵𝛩𝑇』~──❖❱*\n *•┇❖↞استخدم انسحب للانسحاب┇🇸🇦❯*
+    let caption = `*❰❖── ~『𝑀𝐼𝑇𝑺𝑈𝑅𝐼–𝐵𝛩𝑇』~──❖❱*\n *•┇❖↞استخدم انسحب للانسحاب┇🇸🇦❯*
  *•┃❖↞الـوقـت⏳↞* *${(timeout / 1000).toFixed(2)}* *ثانية┇❯*
   
  *•┃❖↞الـجـائـزة💰↞* *${poin}* *نقطه┇❯*
-   *❰❖── ~『 𝑁𝐸𝑍𝑈𝐾𝛩–𝐵𝛩𝑇 』~──❖❱*
+   *❰❖── ~『 𝑀𝐼𝑇𝑺𝑈𝑅𝐼–𝐵𝛩𝑇 』~──❖❱*
      `.trim()
     conn.tokitoki[id] = [
         await conn.sendFile(m.chat, json.img, '', caption, m),
