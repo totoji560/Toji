16afdfe164b3b7d343db4604b86f895733d32656
Amkakira <164623285+Amkakira@users.noreply.github.com>
2024-05-07 20:40:56 +0300

    Update google-img.js

--- plugins/google-img.js
+++ plugins/google-img.js
@@ -1,16 +1,14 @@
 import { googleImage } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `*متنساش ال انت عاوز تبحث عليه يحب , مثال : .صوره شينبو*`
+if (!text) throw `*[❗خطاء❗] مثال علي الامر ${usedPrefix + command} كانيكي*`
+if (m.text.includes('gore') || m.text.includes('cp')|| m.text.includes('porno')|| m.text.includes('Gore')|| m.text.includes('rule')|| m.text.includes('CP')|| m.text.includes('Rule34')) return m.reply('[❗خطاء❗] لا يمكنني إرسال هذا المحتوى ، المجموعة محظورة \n إذا كنت مشرفًا وتريد تنشيطها ، اخبر المطور')  
 const res = await googleImage(text)
-let image = res.getRandom()
+let image = await res.getRandom()
 let link = image
-conn.sendHydrated(m.chat, `🔎 *البحث:* ${text}
-🌎 *المصدر:* Google
-`, author, link, link, '🔗 اللينك', null, null, [
-['🐦 التالي 🐦', `/imagen ${text}`]
-], m)
-}
+conn.sendFile(m.chat, link, 'error.jpg', `🔎 *النتيجه ل:* ${text}\n🔗 *من* ${link}\n🌎 *محرك البحث:* جوجل`, m)}
+//let captionn = `🔎 *𝚁𝙴𝚂𝚄𝙻𝚃𝙰𝙳𝙾 𝙳𝙴:* ${text}\n🔗 *𝙻𝙸𝙽𝙺* ${link}\n🌎 *𝙱𝚄𝚂𝙲𝙰𝙳𝙾𝚁:* Google`
+//conn.sendButton(m.chat, captionn, author, link, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `#imagen ${text}`]], m)}
 handler.help = ['gimage <query>', 'imagen <query>']
 handler.tags = ['internet', 'tools']
-handler.command = /^(صوره|image|بحث-صورة)$/i
+handler.command = /^(gimage|image|صوره|imagen)$/i
 export default handler
