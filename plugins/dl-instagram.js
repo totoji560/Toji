0e3a4a956668195ceebb0782ab586a239c864e00
Amkakira <164623285+Amkakira@users.noreply.github.com>
2024-05-07 20:37:08 +0300

    Update dl-instagram.js

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -1,28 +1,34 @@
-import fetch from 'node-fetch';
-
-let handler = async (m, { conn, usedPrefix, args, command, text }) => {
-  if (!text) throw `الرابط?`;
-  m.reply(wait);
-
-  let res;
-  try {
-    res = await fetch(`https://inrl-web.onrender.com/api/insta?url=${text}`);
-  } catch (error) {
-    throw `حدث خطأ: ${error.message}`;
-  }
-
-  let api_response = await res.json();
-  if (!api_response || !api_response.result || api_response.result.length === 0) {
-    throw `لم يتم العثور على فيديو أو رد غير صالح من API.`;
-  }
-
-  let cap = `هنا الفيديو >,<`;
-
-  conn.sendFile(m.chat, api_response.result[0], 'instagram.mp4', cap, m);
-}
-
-handler.help = ['instagram']
-handler.tags = ['downloader']
-handler.command = /^(instagram|igdl|ig|instagramdl|انستا)$/i
-
+import fetch from 'node-fetch'
+import instagramGetUrl from 'instagram-url-direct'
+import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from '@bochilteam/scraper'
+let handler = async (m, { conn, args, command, usedPrefix }) => {
+if (!args[0]) throw `*لازم تبعت فيديو : ${usedPrefix + command}* https://www.instagram.com/reel/CxGDgFOoG1J/?igshid=NjIwNzIyMDk2Mg=?utm_source=ig_web_copy_link` 
+try {
+await m.reply(`*[❗] ᴅᴇsᴄᴀʀɢᴀɴᴅᴏ sᴜ ᴠɪᴅᴇᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ ᴘᴏʀ ғᴀᴠᴏʀ, ᴇsᴛᴇ ᴘʀᴏᴄᴇsᴏ ᴘᴜᴇᴅᴇ ᴅᴜʀᴀʀ ᴇɴᴛʀᴇ 2 ʏ 10 ᴍɪɴᴜᴛᴏs ᴅᴇᴘᴇɴᴅɪᴇɴᴅᴏ ᴅᴇ ʟᴀ ᴅᴜʀᴀᴄɪᴏɴ ᴅᴇʟ ᴠɪᴅᴇᴏ...*`)
+switch (command) {        
+case "instagramdl": case "instagram": case "igdl": case "ig": 
+let human = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=${lolkeysapi}&url=${args[0]}`)
+let json = await human.json()
+let videoig = json.result
+let shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
+let txt1 = `🔗 *Url:* ${shortUrl1}`.trim()
+await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m)
+break  
+case "instagramdl2": case "instagram2": case "igdl2": case "ig2":        
+const resultss = (await instagramGetUrl(args[0])).url_list[0]
+let shortUrl2 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
+let txt2 = `🔗 *Url:* ${shortUrl2}`.trim()
+conn.sendFile(m.chat, resultss, 'error.mp4', txt2, m)
+break               
+case "instagramdl3": case "instagram3": case "igdl3": case "ig3":            
+const resultssss = await instagramdl(args[0]).catch(async _ => await instagramdlv2(args[0])).catch(async _ => await instagramdlv3(args[0])).catch(async _ => await instagramdlv4(args[0]))
+let shortUrl3 = await (await fetch(`https://tinyurl.com/api-create.php?url=${r.medias[i].url}`)).text()
+let txt3 = `🔗 *Url:* ${shortUrl3}`.trim()
+for (const { url } of resultssss) await conn.sendFile(m.chat, url, 'error.mp4', txt4, m)
+break       
+}} catch {
+await await m.reply(`*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾, 𝚂𝙸 𝙴𝙻 𝙴𝚁𝚁𝙾𝚁 𝚂𝙸𝙶𝚄𝙴, 𝙿𝚁𝚄𝙴𝙱𝙴 𝙲𝙾𝙽 𝙾𝚃𝚁𝙰 𝙾𝙿𝙲𝙸𝙾𝙽 (${usedPrefix}ig, ${usedPrefix}ig2, ${usedPrefix}ig3)*`)
+}}
+handler.command = /^(instagramdl|instagram|igdl|ig|instagramdl2|انستا|igdl2|انستجرام|instagramdl3|instagram3|igdl3|ig3)$/i
 export default handler
+  
