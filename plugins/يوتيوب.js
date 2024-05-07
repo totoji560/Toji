6458035c1962a14c329a506d298d36c8d01051be
Amkakira <164623285+Amkakira@users.noreply.github.com>
2024-05-08 01:09:56 +0300

    Update يوتيوب.js

--- plugins/يوتيوب.js
+++ plugins/يوتيوب.js
@@ -1,20 +1,28 @@
-import fs from 'fs'
-
-let handler = async (m, {conn, text }) => {
-  if (!text) throw '⚠️ *اكتب الامر ويلي تريد تبحث عنه في اليوتيوب*'
-  await conn.reply(m.chat, global.wait, m)
-  let results = await yts(text)
-  let tes = results.all
-  let teks = results.all.map(v => {
-    switch (v.type) {
-      case 'video': return `
-° *_${v.title}_*
-↳ 🫐 *_الرابط :_* ${v.url}
-↳ 🕒 *_الوقت :_* ${v.timestamp}
-↳ 📥 *_اصدر في :_* ${v.ago}
-↳ 👁 *_المشاهدات :_* ${v.views}`}}).filter(v => v).join('\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n\n')
-  conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, m)
-}
-handler.help = ['ytsearch *<texto>*'] 
-handler.tags = ['search']
-handler.command = ['بحث', 'يوتيوب'] 
+import { youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper'
+import fetch from 'node-fetch'
+let handler = async (m, { conn, args }) => {
+if (!args[0]) throw '*هذا الامر خاص بالتحميل من اليوتوب* مثال : \n *.yt* https://youtu.be/MJN8qQObH0s'
+await m.reply(`*جاري تحميل الفيديو المرجو الانتظار قليلا يا عزيزي* `)
+try {
+let qu = args[1] || '720'
+let q = qu + 'p'
+let v = args[0]
+const yt = await youtubedl(v).catch(async _ => await youtubedlv2(v)).catch(async _ => await youtubedlv3(v))
+const dl_url = await yt.video[q].download()
+const ttl = await yt.title
+const size = await yt.video[q].fileSizeH
+await await conn.sendMessage(m.chat, { video: { url: dl_url }, fileName: `${ttl}.mp4`, mimetype: 'video/mp4', caption: `▢: ${ttl}\n▢ 𝙿: ${size}`, thumbnail: await fetch(yt.thumbnail) }, { quoted: m })
+} catch {
+try {
+let lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytvideo2?apikey=85faf717d0545d14074659ad&url=${args[0]}`)    
+let lolh = await lolhuman.json()
+let n = lolh.result.title || 'error'
+let n2 = lolh.result.link
+let n3 = lolh.result.size
+let n4 = lolh.result.thumbnail
+await conn.sendMessage(m.chat, { video: { url: n2 }, fileName: `${n}.mp4`, mimetype: 'video/mp4', caption: `▢ 𝚃: ${n}\n▢ 𝙿: ${n3}`, thumbnail: await fetch(n4) }, { quoted: m })
+} catch {
+await conn.reply(m.chat, 'تعذر تحميل الفيديو راسل ڤينوم لتعرف السبب', m)}
+}}
+handler.command = /^video|فيديو|يوتيوب$/i
+export default handler
