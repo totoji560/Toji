16e61088324c176010c3b0189b81f579dedc3b24
Amkakira <164623285+Amkakira@users.noreply.github.com>
2024-05-09 02:14:27 +0300

    Update الشله.js

--- plugins/الشله.js
+++ plugins/الشله.js
@@ -20,7 +20,7 @@ let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPr
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
     await conn.sendMessage(m.chat, { react: { text: '⚠️', key: m.key } })
-let videoUrl = 'https://telegra.ph/file/c3f4de19475edbbace397.mp4';
+let videoUrl = 'https://telegra.ph/file/2988afee0cd02786c1953.jpg';
   let vn = './media/menu.mp3';
   const user = global.db.data.users[m.sender];
   const {money, joincount} = global.db.data.users[m.sender];
