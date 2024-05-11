992295b687df86413ad4bcb10bb859eeea4d334c
Amkakira <164623285+Amkakira@users.noreply.github.com>
2024-05-11 16:36:22 +0300

    Update م5.js

--- plugins/م5.js
+++ plugins/م5.js
@@ -20,7 +20,7 @@ let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPr
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
     await conn.sendMessage(m.chat, { react: { text: '🎭', key: m.key } })
-let videoUrl = 'https://telegra.ph/file/808a9a7cbf21b2a9456b8.mp4';
+let videoUrl = 'https://telegra.ph/file/8036a2b71408e09b8871a.mp4';
   let vn = './media/menu.mp3';
   const user = global.db.data.users[m.sender];
   const {money, joincount} = global.db.data.users[m.sender];
