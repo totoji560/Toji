62f39abb93bc125527455e773d76f346d80d5d92
Amkakira <164623285+Amkakira@users.noreply.github.com>
2024-05-17 01:01:19 +0300

    Update gc-delete.js

--- plugins/gc-delete.js
+++ plugins/gc-delete.js
@@ -1,19 +1,23 @@
-/* Creditos a https://github.com/FG98F */
-
-
-const handler = async (m, {conn, usedPrefix, command}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins.gc_delete
-
-  if (!m.quoted) throw tradutor.texto1;
+const handler = async (m, { conn, usedPrefix, command }) => {
+  const datas = global;
+  const idioma = datas.db.data.users[m.sender].language;
+  
   try {
-    const delet = m.message.extendedTextMessage.contextInfo.participant;
-    const bang = m.message.extendedTextMessage.contextInfo.stanzaId;
-    return conn.sendMessage(m.chat, {delete: {remoteJid: m.chat, fromMe: false, id: bang, participant: delet}});
-  } catch {
-    return conn.sendMessage(m.chat, {delete: m.quoted.vM.key});
+    const response = await fetch(`./language/${idioma}.json`);
+    const _translate = await response.json();
+    const tradutor = _translate.plugins.gc_delete;
+
+    if (!m.quoted) throw tradutor.texto1;
+    try {
+      const delet = m.message.extendedTextMessage.contextInfo.participant;
+      const bang = m.message.extendedTextMessage.contextInfo.stanzaId;
+      return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: bang, participant: delet } });
+    } catch {
+      return conn.sendMessage(m.chat, { delete: m.quoted.vM.key });
+    }
+  } catch (error) {
+    console.error(error);
+    return conn.sendMessage(m.chat, 'حدث خطأ أثناء قراءة ملف الترجمة.');
   }
 };
 handler.help = ['del', 'delete'];
@@ -23,17 +27,3 @@ handler.group = true;
 handler.admin = true;
 handler.botAdmin = true;
 export default handler;
-
-/* let handler = function (m) {
-if (!m.quoted) throw false
-let { chat, fromMe, isBaileys } = m.quoted
-if (!fromMe) throw false
-if (!isBaileys) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚂𝙴 𝙼𝙴𝙽𝚂𝙰𝙹𝙴 𝙽𝙾 𝙵𝚄𝙴 𝙴𝙽𝚅𝙸𝙰𝙳𝙾 𝙿𝙾𝚁 𝙼𝙸, 𝙽𝙾 𝙻𝙾 𝙿𝚄𝙴𝙳𝙾 𝙴𝙻𝙸𝙼𝙸𝙽𝙰𝚁*'
-conn.sendMessage(chat, { delete: m.quoted.vM.key })
-}
-handler.help = ['del', 'delete']
-handler.tags = ['tools']
-handler.command = /^del(ete)?$/i
-handler.group = true
-handler.admin = true
-export default handler*/
