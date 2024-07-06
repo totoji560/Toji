6d2da186993a672eb0bbe9be03d045322d895fe7
Amkakira <164623285+Amkakira@users.noreply.github.com>
2024-07-06 19:27:10 +0300

    Update questionhal.js

--- plugins/questionhal.js
+++ plugins/questionhal.js
@@ -5,16 +5,16 @@ handler.before = async function(m) {
   const id = m.chat;
   if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/^ⷮ/i.test(m.quoted.text)) return !0;
   this.tekateki = this.tekateki ? this.tekateki : {};
-  if (!(id in this.tekateki)) return m.reply('*❖┃ لـقـد انـتـهـي هـذا السـؤال ┃❗ ❯*');
+  if (!(id in this.tekateki)) return m.reply('*〄┇لــقــد انــتــهــي هــذا الــســؤال┇ ⚜️ ┇➥*');
   if (m.quoted.id == this.tekateki[id][0].id) {
     const json = JSON.parse(JSON.stringify(this.tekateki[id][1]));
     if (m.text.toLowerCase() == json.response.toLowerCase().trim()) {
       global.db.data.users[m.sender].exp += this.tekateki[id][2];
-      m.reply(`*❖┃ اجــابـــة صــحــيــحــة ┃✅ ❯*\n *❖┃الجـايـزة ↞ +${this.tekateki[id][2]} Exp ┃💰 ❯*`);
+      m.reply(`*⧉┇اجــــابـــــة صــــحـــيـــحـــة┇ ☑️ ┇➥*\n *❖┃الجـايـزة ↞ +${this.tekateki[id][2]} Exp ┇ 💰 ❯*`);
       clearTimeout(this.tekateki[id][3]);
       delete this.tekateki[id];
-    } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`*❖┃ اقـتـربـت من الايـجـابـه ┃🔰 ❯*`);
-    else m.reply('*❖┃ إجـــابـــة خــاطــئــة ┃❌ ❯*');
+    } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`*〄┇اقـــتــربــت مــن الـإجـــابـــة┇ 🚸 ┇➥*`);
+    else m.reply('*⌫┇اجــــابـــــة خـــاطــــئــــة┇ ✖️ ┇➥*');
   }
   return !0;
 };
